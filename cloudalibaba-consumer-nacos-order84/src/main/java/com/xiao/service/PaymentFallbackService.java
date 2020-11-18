4b79636acd67f0acd8131e15620325b1b03b0df8
qinghongyou <1917958423@qq.com>
2020-11-18 22:22:04 +0800

    Spring cloud sentinel 流控、降级、熔断

---
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/xiao/service/PaymentFallbackService.java
@@ -0,0 +1,16 @@
+package com.xiao.service;
+
+import com.xiao.entities.CommonResult;
+import com.xiao.entities.Payment;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class PaymentFallbackService implements PaymentService
+{
+    @Override
+    public CommonResult<Payment> paymentSQL(Long id)
+    {
+        return new CommonResult<>(44444,"服务降级返回,---PaymentFallbackService",new Payment(id,"errorSerial"));
+    }
+}
