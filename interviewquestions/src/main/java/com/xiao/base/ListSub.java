224c1390327ccbef4780cd712bdee0dfa728e511
qinghongyou <1917958423@qq.com>
2020-12-22 19:29:44 +0800

    java 面试基础-Java List.subList()方法：获取列表中指定范围的子列表

--- interviewquestions/src/main/java/com/xiao/base/ListSub.java
+++ interviewquestions/src/main/java/com/xiao/base/ListSub.java
@@ -7,18 +7,21 @@ import java.util.List;
 public class ListSub {
     public static void main(String[] args){
         List<String> list = new ArrayList<String>();
-        list.add("JavaWeb编程词典");  //向列表中添加数据
-        list.add("Java编程词典");  //向列表中添加数据
-        list.add("C#编程词典");  //向列表中添加数据
-        list.add("ASP.NET编程词典");  //向列表中添加数据
-        list.add("VC编程词典");  //向列表中添加数据
-        list.add("SQL编程词典");  //向列表中添加数据
+        for(int i=0;i<1500;i++){
+            list.add("JavaWeb编程词典 "+i);
+        }
+          //向列表中添加数据
+        //list.add("Java编程词典");  //向列表中添加数据
+        //list.add("C#编程词典");  //向列表中添加数据
+        //list.add("ASP.NET编程词典");  //向列表中添加数据
+        //list.add("VC编程词典");  //向列表中添加数据
+        //list.add("SQL编程词典");  //向列表中添加数据
         Iterator<String> its = list.iterator();  //获取集合迭代器
         System.out.println("集合中所有元素对象:");
         while(its.hasNext()){  //循环遍历集合
             System.out.print(its.next()+"、");  //输出集合内容
         }
-        List<String>subList = list.subList(3,5);  //获取子列表
+        List<String>subList = list.subList(0,1000);  //获取子列表
         System.out.println("\n截取集合中部分元素:");
         Iterator it = subList.iterator();
         while(it.hasNext()){
