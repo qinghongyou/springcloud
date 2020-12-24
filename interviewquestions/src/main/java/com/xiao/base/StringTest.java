e57064ed1dc1c9792c6ab8c69f74928e698e4603
qinghongyou <1917958423@qq.com>
2020-12-24 12:39:18 +0800

    java 面试基础-测试类

--- interviewquestions/src/main/java/com/xiao/base/StringTest.java
+++ interviewquestions/src/main/java/com/xiao/base/StringTest.java
@@ -1,9 +1,20 @@
 package com.xiao.base;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class StringTest {
 
     public static void main(String[] args) {
         String urlParams= "oauth=ekp&fdMobileType=mobiledingding";
         System.out.println(urlParams.indexOf("mobiledingding")>-1);
+        boolean flagCount = 100>1000;
+        int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);//得到当前时间的小时数
+        if(flagCount && (hour>=8 && hour<=23)){
+            System.out.println("flagCount && (hour>=8 && hour<=23)");
+        }else{
+            System.out.println("else...");
+        }
+        System.out.println(hour);
     }
 }
